a92be356f95a0b8cd092dcc6d98fccc145e7657c
Peter Vestlin <peter.vestlin@lm.se>
2019-06-26 13:00:57 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	kotlin("jvm") version "1.3.40"
+}
+
+group = "nu.westlin"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	
+	testCompile("org.junit.jupiter:junit-jupiter:5.4.2")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
---
+++ src/main/kotlin/nu/westlin/result/Result.kt
@@ -0,0 +1,2 @@
+package nu.westlin.result
+
